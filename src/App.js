5dfcadbbae77fa3555a6b66f6a65327a938e75fa
AvishkarMandlik <mandlikavi121@gmail.com>
2022-12-22 15:58:00 +0530

    front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState,useEffect } from 'react'
+
+import "./App.css"
+
+import pngGmap from "./location.png"
 
 function App() {
+
+  const [city,setCity] = useState("Banglore")
+  const [temp,setTemp] = useState(0)
+  const [description,setDescription] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="heading-container"><h1 className="app-title"> <i>Weather App</i></h1></div>
+    
+    <div className='search-container'>
+    <img src={pngGmap} className="img-location" />
+    <input type="text" placeholder="Enter City Name" 
+    className="input-city" value={city}
+    onChange={(e)=> setCity(e.target.value)}/>
     </div>
-  );
+
+    <div>
+      <h3>{city}</h3>
+    </div>
+
+    </>
+  )
 }
 
-export default App;
+export default App
