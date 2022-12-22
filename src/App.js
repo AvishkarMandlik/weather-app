c0301fb500381741625bfc129e94135c431c7026
AvishkarMandlik <mandlikavi121@gmail.com>
2022-12-22 23:52:12 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useState,useEffect } from 'react'
 import axios from "axios";
 
 import "./App.css"
-
 import pngGmap from "./location.png"
 
 function App() {
@@ -12,13 +11,15 @@ function App() {
   const [temp,setTemp] = useState(0)
   const [description,setDescription] = useState("")
 
+
   useEffect(()=>{
     async function loadData(){
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=73ba4ed59fd3b370d4f178d831d3833a`)
       if(response.status === 200){
         const temp = Math.round(response.data.main.temp - 273.15);
         setTemp(`${temp}°C`);
-        
+        const description = response.data.weather[0].description
+        setDescription(description)
       }
 
     }
@@ -27,20 +28,22 @@ function App() {
 
   return (
     <>
+    <div className="background">
     <div className="heading-container"><h1 className="app-title"> <i>Weather App</i></h1></div>
     
     <div className='search-container'>
-    <img src={pngGmap} className="img-location" />
+    <img src={pngGmap} className="img-location" alt="pngGmap"/>
     <input type="text" placeholder="Enter City Name" 
     className="input-city" value={city}
     onChange={(e)=> setCity(e.target.value)}/>
     </div>
 
     <div className="weather-container">
-      <h2 className='city'>CITY :- {city}</h2>
-      <h2 className='city-tempreture'>Tempreture :- {temp}</h2>
+      <div className='city'>City :- {city}</div>
+      <div className='city-tempreture'>Tempreture :- {temp}</div>
+      <div className='city-description'>Description :- {description} </div>
     </div>
-
+    </div>
     </>
   )
 }
